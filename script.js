03fdb74b9cf737e2131677c7ba1ee372e601a0ae
HopeMani17 <88143139+HopeMani17@users.noreply.github.com>
2025-02-20 10:56:25 +1100

    Added js code

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
+
+document.body.style.background = "black";
+
+const textContainer = document.querySelector("#textContainer");
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "Welcome to Rock, Paper & Scissors Game!";
+content.style.color="white";
+container.appendChild(content);
+
+// buttons is a node list. It looks and acts much like an array.
+const button=document.querySelectorAll("button");
+
+const buttonOne = document.querySelector("#one");
+buttonOne.addEventListener("click", gethumanSelection);
+
+const buttonTwo = document.querySelector("#two");
+buttonTwo.addEventListener("click", getcomputerSelection);
+
+const buttonThree = document.querySelector("#three");
+buttonThree.addEventListener("click", playGame);
+
+//Function to get value for computer
 function getcomputerSelection()
 {
-//  let finalValue = prompt("Computer, Please enter your choice:(rock, paper, or scissors):").toLowerCase();
-    let  finalValue= Math.floor(Math.random()*3);
-    if(finalValue<1)
+  const randomValue= Math.floor(Math.random()*3);
+    if(randomValue<1)
         {
             return "rock";
         }
-        else if(finalValue>1)
+        else if(randomValue>1)
         {
             return "paper";
         }
@@ -16,63 +38,68 @@ function getcomputerSelection()
         }
 }
 
+//Function to get value for human player
 function gethumanSelection()
 {
-    let humanInput = prompt("Please enter your choice:(rock, paper, or scissors):").toLowerCase();
+    let humanInput = prompt("Please enter your choice:(rock, paper, or scissors):");
     return humanInput;
 }
-
+const humanSelection = gethumanSelection();
+const computerSelection = getcomputerSelection();
+//Function to play game
+  
  function playGame()
  {
-  
- let humanScore=0;
- let computerScore=0;
- const humanSelection = gethumanSelection();
- const computerSelection = getcomputerSelection();
- 
-function playRound(humanChoice, computerChoice)
-{
+    let humanScore=0;
+    let computerScore=0;
     
-    if(humanChoice === computerChoice)
-        {
-            return "it is a tie. you both picked ${humanSelection}";
+    if(computerSelection === humanSelection)
+        { 
+            const output = document.createElement("div");
+            output.classList.add("output");
+            output.textContent = "it is a tie. You both picked same item";
+            output.style.color="white";
+            container.appendChild(output);  
         }
-        else if(humanChoice === "rock" && computerChoice === "scissors")
+        else if(humanSelection === "rock" && computerSelection === "scissors")
         {
-            return "You win! rock beats scissor";
+            const output = document.createElement("div");
+            output.classList.add("output");
+            output.textContent = "You win! Rock beats Scissor";
+            output.style.color="white";
+            container.appendChild(output); 
             humanScore++;
         }
-        else if(humanChoice === "rock" && computerChoice === "paper")
-        {
-            return "You win! paper beats rock";
+        else if(humanSelection === "paper" && computerSelection === "rock")
+        { 
+            const output = document.createElement("div");
+            output.classList.add("output");
+            output.textContent = "You win! Paper beats Rock";
+            output.style.color="white";
+            container.appendChild(output); 
             humanScore++;
         }
-         else if(humanChoice === "scissors" && computerChoice === "paper")
+         else if(humanSelection === "scissors" && computerSelection === "paper")
         {
-            return "You win! scissors beats paper";
+            const output = document.createElement("div");
+            output.classList.add("output");
+            output.textContent = "You win! Scissors beats Paper";
+            output.style.color="white"; container.appendChild(output); 
             humanScore++;
         }
         else
-        {
-            return "you lose! ${humanChoice} beats ${humanSelection}";
+        {   const output = document.createElement("div");
+            output.classList.add("output");
+            output.textContent = "You lose! Computer selection beats you";
+            output.style.color="white"; 
+            container.appendChild(output); 
             computerScore++;
         }
-     
-}
-
-
-
-
-
-
- for(let i=0;i<5;i++)
-      {
-          playRound(humanSelection, computerSelection);
-          console.log("Final score: " + humanScore + " to " + computerScore + "");
-      }
-    }
-
- 
-
- 
-
+    
+        const output = document.createElement("div");
+        output.classList.add("output");
+        output.textContent = "Final score: " + humanScore + " to " + computerScore + "";
+        output.style.color="white"; 
+        container.appendChild(output);
+    
+ }
